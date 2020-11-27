95b1d272c1a22d7e2c41ada9205adfc029287508
JorgeVega97 <jorgeenriquevegamaradiaga6@gmail.com>
2020-11-27 11:24:19 -0600

    Clases detective y caso

--- Lab2Repo_JorgeVega/src/lab2repo_jorgevega/CLase_detectives.java
+++ Lab2Repo_JorgeVega/src/lab2repo_jorgevega/CLase_detectives.java
@@ -4,5 +4,61 @@ package lab2repo_jorgevega;
 Jorge Vega
  */
 public class CLase_detectives {
-   
+    private String NOMBRE,NACIONALIDAD;
+    private int EDAD,ANTIGUEDAD,NIVEL;
+
+    public CLase_detectives(String NOMBRE, String NACI, int EDAD, int ANTIGUEDAD, int LEVEL) {
+        this.NOMBRE = NOMBRE;
+        this.NACIONALIDAD = NACI;
+        this.EDAD = EDAD;
+        this.ANTIGUEDAD = ANTIGUEDAD;
+        this.NIVEL = LEVEL;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+
+    public void setNOMBRE(String NOMBRE) {
+        this.NOMBRE = NOMBRE;
+    }
+
+    public String getNACIONALIDAD() {
+        return NACIONALIDAD;
+    }
+
+    public void setNACIONALIDAD(String NACIONALIDAD) {
+        this.NACIONALIDAD = NACIONALIDAD;
+    }
+
+    public int getEDAD() {
+        return EDAD;
+    }
+
+    public void setEDAD(int EDAD) {
+        this.EDAD = EDAD;
+    }
+
+    public int getANTIGUEDAD() {
+        return ANTIGUEDAD;
+    }
+
+    public void setANTIGUEDAD(int ANTIGUEDAD) {
+        this.ANTIGUEDAD = ANTIGUEDAD;
+    }
+
+    public int getNIVEL() {
+        return NIVEL;
+    }
+
+    public void setNIVEL(int NIVEL) {
+        this.NIVEL = NIVEL;
+    }
+    
+    
+    
+    @Override
+    public String toString(){
+        return "NOMBRE: "+NOMBRE+" EDAD: "+EDAD+" NACIONALIDAD: "+NACIONALIDAD+" ANTIGUEDAD LABORAL: "+ANTIGUEDAD+" NIVEL: "+NIVEL;
+    }
 }
--- Lab2Repo_JorgeVega/src/lab2repo_jorgevega/Clase_casos.java
+++ Lab2Repo_JorgeVega/src/lab2repo_jorgevega/Clase_casos.java
@@ -4,5 +4,40 @@ package lab2repo_jorgevega;
 Jorge Vega
  */
 public class Clase_casos {
-    
+    private String TIPO,ESTADO, NOMBRE;
+
+    public Clase_casos(String TIPO, String ESTADO, String NOMBRE) {
+        this.TIPO = TIPO;
+        this.ESTADO = ESTADO;
+        this.NOMBRE = NOMBRE;
+    }
+
+    public String getTIPO() {
+        return TIPO;
+    }
+
+    public void setTIPO(String TIPO) {
+        this.TIPO = TIPO;
+    }
+
+    public String getESTADO() {
+        return ESTADO;
+    }
+
+    public void setESTADO(String ESTADO) {
+        this.ESTADO = ESTADO;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+
+    public void setNOMBRE(String NOMBRE) {
+        this.NOMBRE = NOMBRE;
+    }
+
+    @Override
+    public String toString(){
+        return "TIPO: "+TIPO+" DETECTIVE A CARGO: "+NOMBRE+" ESTADO: "+ESTADO;
+    }
 }
